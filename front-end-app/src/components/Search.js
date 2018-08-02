f81b4d541143da23a0c1e6feff1c5935fba65fcf
KydneyBean <kydney.been@gmail.com>
2018-08-02 14:57:55 -0700

    SY - Got Location to Work

--- front-end-app/src/components/Search.js
+++ front-end-app/src/components/Search.js
@@ -1,9 +1,61 @@
 import React, {Component } from 'react'
+import axios from "axios"
 
 class Search extends Component {
+    state = {
+        query:null
+    }
+
+    getAddress = (city) => {
+        this.props.filteredEvents(this.state.query, city)
+    }
     render() {
         return (
-             <h1>Search Component</h1>
+            <div className ="row">
+                <div className ="col">
+                    <Input />
+                </div>
+                <div className ="col">
+                    <Location getAddress={this.getAddress}/>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Input extends Component {
+    render() {
+        return (
+            null
+        )
+    }
+}
+
+class Location extends Component {
+    state = {
+        city:"",
+        province:"",
+    }
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            let latlng = pos.coords.latitude + "," + pos.coords.longitude
+            axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlng}&key=AIzaSyBeBWzf10BUjCAqbTY3qbjDdPjHOCTx_eM`)
+                .then((res) => {
+                let city = res.data.results[0].address_components[3].long_name
+                let province = res.data.results[0].address_components[5].short_name
+                this.setState({
+                    city,
+                    province,
+                })
+            })
+        })
+    }
+    render() {
+        this.props.getAddress(this.state.city, this.state.province)
+        return (
+            <div>
+            {this.state.city}, {this.state.province}
+            </div>
         )
     }
 }
