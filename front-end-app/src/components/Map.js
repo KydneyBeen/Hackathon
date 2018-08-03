274a0c2c798fc20cfdc574fe0b8f64493d44f004
KydneyBean <kydney.been@gmail.com>
2018-08-03 13:29:19 -0700

    Map

--- front-end-app/src/components/Map.js
+++ front-end-app/src/components/Map.js
@@ -8,9 +8,9 @@ class Map extends Component {
 
         return (
             <div>
-                {!this.props.currEvent.address ? <p>Choose and event to see this map...</p> :
+                {!this.props.currEvent ? <p>Choose and event to see this map...</p> :
                     <div>
-                        <iframe className= 'map-iframe' src={`https://www.google.com/maps/embed/v1/search?q=${this.props.currEvent.address.stNameAndNumber || "Canada"}&key=AIzaSyD6Y-jvO7HIRAAwTKIWiVcAZJf8ywR1MDs`} allowFullScreen title="Google Map"></iframe> 
+                        <iframe className= 'map-iframe' src={`https://www.google.com/maps/embed/v1/search?q=${this.props.currEvent._embedded.venues[0].address.line1 || "Canada"}&key=AIzaSyD6Y-jvO7HIRAAwTKIWiVcAZJf8ywR1MDs`} allowFullScreen title="Google Map"></iframe> 
                     </div>
                 }
             </div>
