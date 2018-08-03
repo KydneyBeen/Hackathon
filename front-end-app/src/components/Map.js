2484b6f7acde0e3c8085c266978b92253832c97f
KydneyBean <kydney.been@gmail.com>
2018-08-02 19:47:28 -0700

    Map works

--- front-end-app/src/components/Map.js
+++ front-end-app/src/components/Map.js
@@ -1,9 +1,16 @@
 import React, {Component } from 'react'
+import axios from "axios"
 
 class Map extends Component {
-    render() {
+    state= {
+        address: "455 Granville St"
+    }
+
+    render() {        
         return (
-             <h1>Map Component</h1>
+             <div>
+                <iframe src={`https://www.google.com/maps/embed/v1/search?q=${this.state.address}&key=AIzaSyD6Y-jvO7HIRAAwTKIWiVcAZJf8ywR1MDs`} allowFullScreen title="Google Map"></iframe> 
+            </div>
         )
     }
 }
